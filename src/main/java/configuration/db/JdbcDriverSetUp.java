173440b15ad9c35173883d96c9784e197c72e8fe
stesho01 <olga_steshenko@epam.com>
2022-08-15 00:03:17 +0300

    add test for jdbc

--- src/main/java/configuration/db/JdbcDriverSetUp.java
+++ src/main/java/configuration/db/JdbcDriverSetUp.java
@@ -1,2 +1,37 @@
-package configuration.db;public class JdbcDriverSetUp {
+package configuration.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class JdbcDriverSetUp {
+    private static final String JDBC_DRIVER = "com.mysql.cj.jdbc.Driver";
+    private static final String DATABASE_URL = "jdbc:mysql://localhost:3306/HomeTask";
+    private static final String USER = "root";
+    private static final String PASSWORD = "temp12345";
+    private Connection connection;
+
+    public void dbDriverSetUp() {
+        try{
+            Class.forName(JDBC_DRIVER);
+         } catch(ClassNotFoundException e) {
+            System.out.println("Unable to load class.");
+            e.printStackTrace();
+            return;
+        }
+
+        try{
+            connection = DriverManager.getConnection(DATABASE_URL, USER, PASSWORD);
+        } catch(SQLException e)
+        {
+            e.printStackTrace();
+            return;
+        }
+
+    }
+
+    public Connection getConnection(){
+        return this.connection;
+    }
+
 }
--- src/test/java/com/cucumber/junit/db/TestDB.java
+++ src/test/java/com/cucumber/junit/db/TestDB.java
@@ -1,2 +1,70 @@
-package com.cucumber.junit.db;public class TestDB {
+package com.cucumber.junit.db;
+
+import configuration.db.JdbcDriverSetUp;
+import org.assertj.core.api.Assertions;
+import org.junit.Test;
+import org.junit.jupiter.api.DisplayName;
+
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.*;
+
+import static org.junit.Assert.assertNotNull;
+
+public class TestDB {
+
+    @Test
+    @DisplayName("Read values from one table in User db and check it contains specific value")
+    public void simpleTestDB() throws SQLException {
+        final JdbcDriverSetUp jdbcDriverSetUp = new JdbcDriverSetUp();
+        jdbcDriverSetUp.dbDriverSetUp();
+
+        Connection connection = jdbcDriverSetUp.getConnection();
+        assertNotNull(connection);
+
+        Statement stm = connection.createStatement();
+        String query = "select Name from users;";
+        ResultSet resultSet = stm.executeQuery(query);
+        List<String> namesList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String username = resultSet.getString("Name");
+            namesList.add(username);
+        }
+
+        System.out.println(namesList);
+        Assertions.assertThat(namesList.contains("Alex")).overridingErrorMessage("There are no such entries in the db").isTrue();
+        resultSet.close();
+        connection.close();
+        stm.close();
+    }
+
+    @Test
+    @DisplayName("Read values from two tables in User db")
+    public void testQueryWithJoin() throws SQLException {
+        final JdbcDriverSetUp jdbcDriverSetUp = new JdbcDriverSetUp();
+        jdbcDriverSetUp.dbDriverSetUp();
+
+        Connection connection = jdbcDriverSetUp.getConnection();
+        assertNotNull(connection);
+
+        Statement stm = connection.createStatement();
+        String query = "select us.Name, us.Surname, us.email, ord.order_id, ord.order_total from users  as us join orders as ord where  ord.username = us.Name;";
+        ResultSet resultSet = stm.executeQuery(query);
+        List<String> namesList = new ArrayList<>();
+
+        while (resultSet.next()) {
+            String username = resultSet.getString("Name");
+            namesList.add(username);
+        }
+
+        Assertions.assertThat(namesList.isEmpty()).overridingErrorMessage("There are matches between orders and users").isFalse();
+
+        resultSet.close();
+        connection.close();
+        stm.close();
+    }
+
 }
